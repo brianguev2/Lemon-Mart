befa0f66fa2ee6d2659556da856ed6ba9ebec13a
Brian Guevara <brianguev2@gmail.com>
2018-10-06 22:54:45 -0400

    Created Swagger Server

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,11 +5,13 @@ import { UserRoutingModule } from './user-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { LogoutComponent } from './logout/logout.component';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
+import { MaterialModule } from './../material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    MaterialModule
   ],
   declarations: [ProfileComponent, LogoutComponent, NavigationMenuComponent]
 })

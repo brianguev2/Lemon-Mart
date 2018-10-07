617308745d750d129e8ddaa16c894c10cb12fc85
Brian Guevara <brianguev2@gmail.com>
2018-10-06 20:40:15 -0400

    Lazy loaded + CreatedCom + ChildRouting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <mat-toolbar color="primary" fxLayoutGap="8px">
+  <button mat-icon-button><mat-icon>menu</mat-icon></button>
+  <a mat-icon-button routerLink="/home"><mat-icon svgIcon="squeezer"></mat-icon><span class="mat-h2">LemonMart</span></a>
+  <span class="flex-spacer"></span>
+  <button mat-mini-fab routerLink="/user/profile" matTooltip="Profile" aria-label="User Profile"><mat-icon>account_circle</mat-icon>
+  </button>
+  <button mat-mini-fab routerLink="/user/logout" matTooltip="Logout" aria-label="Logout"><mat-icon>lock_open</mat-icon></button>
+</mat-toolbar>
+<router-outlet></router-outlet>
+
+  <!-- <div>Icons made by <a href="http://www.freepik.com"
+  title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"
+  title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"
+  title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> --> `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lemon-mart';
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'squeezer',
+      sanitizer.bypassSecurityTrustResourceUrl('/assets/img/icons/squeezer.svg')
+    );
+  }
 }
---
+++ src/app/inventory/inventory-routing.module.ts
@@ -0,0 +1,27 @@
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductsComponent } from './products/products.component';
+import { InventoryComponent } from './inventory.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { StockEntryComponent } from './stock-entry/stock-entry.component';
+import { InventoryHomeComponent } from './inventory-home/inventory-home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: InventoryComponent,
+    children: [
+      { path: '', redirectTo: '/inventory/home', pathMatch: 'full' },
+      { path: 'home', component: InventoryHomeComponent },
+      { path: 'stockEntry', component: StockEntryComponent },
+      { path: 'products', component: ProductsComponent},
+      { path: 'categories', component: CategoriesComponent}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class InventoryRoutingModule { }
